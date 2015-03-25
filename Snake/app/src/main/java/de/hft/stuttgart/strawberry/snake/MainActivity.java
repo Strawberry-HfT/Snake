ec8a30e902052174aacc946a28d8014659d27e5f
Constantin <12seco1bwi@hft-stuttgart.de>
2015-03-25 21:28:39 +0100

    Hauptmenü-Buttons hinzugefügt

--- Snake/app/src/main/java/de/hft/stuttgart/strawberry/snake/MainActivity.java
+++ Snake/app/src/main/java/de/hft/stuttgart/strawberry/snake/MainActivity.java
@@ -4,14 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Buttons
+    Button singleplayer;
+    Button multiplayer;
+    Button exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        singleplayer = (Button) findViewById(R.id.singleplayer);
+        singleplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 
 
